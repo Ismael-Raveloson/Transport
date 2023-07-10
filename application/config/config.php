882a5dc4ac9cc79eab00c9ea033c9d884508592b
Ismael-Raveloson <120553853+Ismael-Raveloson@users.noreply.github.com>
2023-07-10 05:23:50 +0300

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://transport-zeta-self.vercel.app/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://transport-zeta-self.vercel.app/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'https://transport-zeta-self.vercel.app/Welcome/index';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
